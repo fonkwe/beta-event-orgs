35c2fae204e40e5c41f6d759815bc5a3dd095d04
Desmond <desmondtekum9@gmail.com>
2022-07-02 17:36:23 +0100

    Ticketer

---
+++ src/About.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import man1 from "../assets/man1.jpg";
+import man2 from "../assets/man2.jpeg";
+import man3 from "../assets/man3.svg";
+
+function About() {
+  return (
+    <div>
+        <div class="about-section">
+  <h1>About Us Page</h1>
+  <p>Some text about who we are and what we do.</p>
+  <p>Resize the browser window to see that this page is responsive by the way.</p>
+</div>
+
+<h2 class="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src={man3} alt="Jane" width={100} />
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO & Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>jane@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src={man2} alt="Mike" width = {100} / >
+      <div class="container">
+        <h2>Mike Ross</h2>
+        <p class="title">Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>mike@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src={man1}alt="John" width ={100} />
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>john@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import login from "./login";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+    <Router>
+    <Routes>
+    <Route path='/login' element = {<login />} />
+    <Route path='/About' element = {<About />} />
+    
+
+    </Routes>
+    
+    </Router>
+   
     </div>
-  );
+  )
 }
 
 export default App;
