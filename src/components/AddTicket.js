5d566bb1d494edbd7a82101ee632ff7a9f756134
fonkwe <williajeni@gmail.com>
2022-07-02 10:49:52 -0500

    added navbar and hero section

---
+++ src/components/AddTicket.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function AddTicket() {
+  return (
+    <div>
+        <Container>
+            <Row>
+                <Col>
+                  <h1>New Ticket</h1>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                  <h1>New Ticket</h1>
+                </Col>
+            </Row>
+
+        </Container>
+      
+    </div>
+  )
+}
